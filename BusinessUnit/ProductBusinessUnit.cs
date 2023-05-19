01e3162e85fb2713609b68e02b6ceff4446b7100
burakcakir <burakckr@windowslive.com>
2023-05-19 17:35:30 +0300

    - ProductController added

---
+++ BusinessUnit/ProductBusinessUnit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Auction_API.Infrastructure.Dto;
+using Auction_Project.DataAccess;
+using Auction_Project.Infrastructure;
+using Auction_Project.Infrastructure.Dto;
+using Auction_Project.Infrastructure.Entity;
+
+namespace Auction_API.BusinessUnit
+{
+    public interface IProductBusinessUnit
+    {
+        Task<List<Product>> GetUserProductsList(int userId);
+    }
+
+    public class ProductBusinessUnit : IProductBusinessUnit
+	{
+
+        private readonly IProductDataAccess _productDataAccess;
+
+        public ProductBusinessUnit(IProductDataAccess productDataAccess)
+        {
+            _productDataAccess = productDataAccess;
+        }
+
+        public Task<List<Product>> GetUserProductsList(int userId)
+        {
+            return _productDataAccess.GetUserProductsList(userId);
+        }
+    }
+}
+
